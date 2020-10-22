1306ad720db0fd671cf1be460ca6c9b1bcdf32c8
Aadarsh Vinayak <aadarshv98@gmail.com>
2020-10-22 01:27:21 -0400

    Changed the titel screen to allow for icons

--- SIMON V2/Assets/Scripts/Scaler.cs
+++ SIMON V2/Assets/Scripts/Scaler.cs
@@ -7,7 +7,7 @@ public class Scaler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float f = 6f / Screen.currentResolution.width;
+        float f = 6f / Screen.currentResolution.height;
         gameObject.GetComponent<RectTransform>().localScale = new Vector3(f, f, 0);
     }
 }
