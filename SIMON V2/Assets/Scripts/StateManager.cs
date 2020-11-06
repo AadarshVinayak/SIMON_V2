e246e1f92a0f42ff0cd52a272f1426098fcc2806
Aadarsh Vinayak <aadarshv98@gmail.com>
2020-11-06 01:46:54 -0500

    Shop anim created

--- SIMON V2/Assets/Scripts/StateManager.cs
+++ SIMON V2/Assets/Scripts/StateManager.cs
@@ -4,16 +4,55 @@ using UnityEngine;
 
 public class StateManager : MonoBehaviour
 {
-    string currentState;
+
+    Animation shop;
+    Animation settings;
+    Animation main;
+
+
+    (string state, Animation ani) currentState;
+    string newState;
 
     private void Start()
     {
-
-        currentState = "Main";
+        shop = GameObject.FindGameObjectWithTag("Shop").GetComponent<Animation>();
+        settings = GameObject.FindGameObjectWithTag("Settings").GetComponent<Animation>();
+        main = GameObject.FindGameObjectWithTag("Main").GetComponent<Animation>();
+        currentState = ("Main", main);
+        newState = "Main";
     }
 
     public void SetState(string s)
     {
-        currentState = s;
+        Debug.Log("Worked: " + s);
+        newState = s;
+    }
+
+    public void Update()
+    {
+        if(currentState.state != newState)
+        {
+            switch (newState)
+            {
+                case "Main":
+                    Debug.Log("Main: " + currentState.ToString());
+                    main.Play("UIEnterLeft");
+                    currentState.ani.Play("UILeaveRight");
+                    currentState = (newState, main);
+                    break;
+                case "Shop":
+                    Debug.Log("Shop: " + currentState.ToString());
+                    shop.Play("UIEnter");
+                    currentState.ani.Play("UILeave");
+                    currentState = (newState, shop);
+                    break;
+                case "Settings":
+                    Debug.Log("Settings: " + currentState.ToString());
+                    settings.Play("UIEnter");
+                    currentState.ani.Play("UILeave");
+                    currentState = (newState, settings);
+                    break;
+            }
+        }
     }
 }
